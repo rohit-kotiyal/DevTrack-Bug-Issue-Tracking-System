8f0bbee600c40e34e996f77e3e73fe6c62137d42
rohit-kotiyal <168202438+rohit-kotiyal@users.noreply.github.com>
2026-01-25 21:18:28 +0530

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,5 +3,8 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  base: "", // required for Render static hosting
+  base: "/",         
+  build: {
+    assetsDir: "assets",
+  },
 });
