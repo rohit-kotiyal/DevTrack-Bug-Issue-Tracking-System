cd4d747e9e785f6b42bd6a641c10c6952601cc71
rohit-kotiyal <168202438+rohit-kotiyal@users.noreply.github.com>
2026-01-26 14:13:36 +0530

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,20 +1,3 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Login from "./pages/Login";
-import Register from "./pages/Register";
-import Projects from "./pages/Projects";
-import ProjectTickets from "./pages/ProjectTickets";
-
-function App() {
-  return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/login" element={<Login />} />
-        <Route path="/register" element={<Register />} />
-        <Route path="/projects" element={<Projects />} />
-        <Route path="/projects/:projectIdAndName" element={<ProjectTickets />} />
-      </Routes>
-    </BrowserRouter>
-  );
+export default function App() {
+  return <h1>Hello React UI</h1>;
 }
-
-export default App;
