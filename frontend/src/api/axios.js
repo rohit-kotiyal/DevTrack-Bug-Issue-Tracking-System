ab0d8d459085af3f0fda3432076e517ba5ee99bb
rohit-kotiyal <168202438+rohit-kotiyal@users.noreply.github.com>
2026-01-26 12:03:03 +0530

    Update axios.js

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,8 +1,9 @@
 import axios from "axios";
 
 const instance = axios.create({
-  baseURL: "http://localhost:8000",
+  import.meta.env.VITE_API_URL || "http://localhost:8000",
   withCredentials: true,
 });
 
 export default instance;
+
